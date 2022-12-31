e329dfcda326621ed32cd467186ec2d5fb8ba915
Shaffaf <shaffaf@orchiderp.com>
2022-12-31 20:32:00 +0400

    zip code modules added

--- heiniger_addons/models/__init__.py
+++ heiniger_addons/models/__init__.py
@@ -1,11 +1,3 @@
 # # -*- coding: utf-8 -*-
 
-# from . import partner
-# from . import partner_masters
-# from . import product
-# from . import product_masters
-# from . import account
-# from . import stock
-# from . import sale
-# from . import old_erp
-# # from . import account_invoice
+# from . import crm_lead
